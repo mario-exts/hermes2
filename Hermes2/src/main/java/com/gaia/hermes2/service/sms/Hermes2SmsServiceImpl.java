b3861f8676c5a4c86271c5ba7eaa4f9c591d4400
Bach Hoang Nguyen <hoangbach.bk@gmail.com>
2016-05-27 09:45:14 +0700

    ...

--- Hermes2/src/main/java/com/gaia/hermes2/service/sms/Hermes2SmsServiceImpl.java
+++ Hermes2/src/main/java/com/gaia/hermes2/service/sms/Hermes2SmsServiceImpl.java
@@ -21,7 +21,7 @@ public class Hermes2SmsServiceImpl extends Hermes2AbstractSmsService {
 	public void init(PuObjectRO properties) {
 		this.clientConfig = properties.getPuObject(F.CLIENT_CONFIG, new PuObject());
 		this.applicationConfig = properties.getPuObject(F.APPLICATION_CONFIG, new PuObject());
-		getLogger().debug("appconfig: "+applicationConfig);
+		getLogger().debug("appconfig: " + applicationConfig);
 		this.asyncSender = new SmsAsyncSender(applicationConfig.getString(F.ACCESS_TOKEN));
 		this.executor = Executors.newCachedThreadPool(new ThreadFactoryBuilder()
 				.setNameFormat("Hermes2GCM " + applicationConfig.getString(F.ID) + " #%d").build());
@@ -30,41 +30,41 @@ public class Hermes2SmsServiceImpl extends Hermes2AbstractSmsService {
 	@Override
 	public void close() throws IOException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void sendSms(SmsContent content) {
-		Callback<SendResult> successCallback=new Callback<SendResult>() {
+		Callback<SendResult> successCallback = new Callback<SendResult>() {
 
 			@Override
 			public void apply(SendResult result) {
-				getLogger().debug("Sending SMS success: "+result.getMessage());
-				
+				getLogger().debug("Sending SMS success: " + result.getMessage());
+
 			}
-			
+
 		};
-		Callback<Throwable> failureCallback=new Callback<Throwable>() {
-			
+		Callback<Throwable> failureCallback = new Callback<Throwable>() {
+
 			@Override
 			public void apply(Throwable cause) {
 				getLogger().error("An error occur when sending SMS: ", cause);
-//				executor.submit(new Runnable() {
-//
-//					@Override
-//					public void run() {
-//					}
-//				});
+				// executor.submit(new Runnable() {
+				//
+				// @Override
+				// public void run() {
+				// }
+				// });
 			}
 		};
-		 asyncSender.sendSMS(content, successCallback, failureCallback);
-		
+		asyncSender.sendSMS(content, successCallback, failureCallback);
+
 	}
 
 	@Override
 	public void checkSendStatus(String transId) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
